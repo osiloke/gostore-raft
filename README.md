179ceddedafad909cb80f0188399a10f9e99b8e7
Osiloke Emoekpere <osi@progwebtech.com>
2022-12-13 14:10:30 +0100

    feat: extract fsm

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gostore-raft
+
 Raft implementation of gostore for horizontal scaling gostore stores
+
+###
+
+start a cluster
+
+`./stored --raftAddr=127.0.0.1:5001 --nodeID=node1   `
+`./stored --raftAddr=127.0.0.1:5001 --nodeID=node1   `
+`./stored --raftAddr=127.0.0.1:5001 --nodeID=node1   `
+
+call a store endpoint
+
+```
+ ~/go/bin/cli call go.micro.api.raft Store.Get '{"key": "hello"}'
+```
--- store/store.go
+++ store/store.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"log"
 	"net"
@@ -140,7 +139,7 @@ func (s *DefaultStore) Open(enableSingle bool) error {
 	return nil
 }
 
-//Bootstrap bootstraps a cluster of known nodes
+// Bootstrap bootstraps a cluster of known nodes
 func (s *DefaultStore) Bootstrap(nodes [][]string) error {
 	servers := make([]raft.Server, len(nodes))
 	for i, n := range nodes {
@@ -154,7 +153,7 @@ func (s *DefaultStore) Bootstrap(nodes [][]string) error {
 	return nil
 }
 
-//Leave cluster
+// Leave cluster
 func (s *DefaultStore) Leave(peerId string) error {
 	if s.raft.State() != raft.Leader {
 		return errors.New("not the leader")
@@ -266,97 +265,3 @@ func (s *DefaultStore) Stats() map[string]string {
 func (s *DefaultStore) Close() {
 
 }
-
-type fsm DefaultStore
-
-// Apply applies a Raft log entry to the key-value store.
-func (f *fsm) Apply(l *raft.Log) interface{} {
-	var c command
-	if err := json.Unmarshal(l.Data, &c); err != nil {
-		panic(fmt.Sprintf("failed to unmarshal command: %s", err.Error()))
-	}
-
-	switch c.Op {
-	case "set":
-		return f.applySet(c.Key, c.Value)
-	case "delete":
-		return f.applyDelete(c.Key)
-	default:
-		panic(fmt.Sprintf("unrecognized command op: %s", c.Op))
-	}
-}
-
-// Snapshot returns a snapshot of the key-value store.
-func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-
-	// Clone the map.
-	o := make(map[string]string)
-	for k, v := range f.m {
-		o[k] = v
-	}
-	return &fsmSnapshot{store: o}, nil
-}
-
-// Restore stores the key-value store to a previous state.
-func (f *fsm) Restore(rc io.ReadCloser) error {
-	o := make(map[string]string)
-	if err := json.NewDecoder(rc).Decode(&o); err != nil {
-		return err
-	}
-
-	// Set the state from the snapshot, no lock required according to
-	// Hashicorp docs.
-	f.m = o
-	return nil
-}
-
-func (f *fsm) applySet(key, value string) interface{} {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	f.m[key] = value
-	return nil
-}
-
-func (f *fsm) applyDelete(key string) interface{} {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	delete(f.m, key)
-	return nil
-}
-
-type fsmSnapshot struct {
-	store map[string]string
-}
-
-func (f *fsmSnapshot) Persist(sink raft.SnapshotSink) error {
-	err := func() error {
-		// Encode data.
-		b, err := json.Marshal(f.store)
-		if err != nil {
-			return err
-		}
-
-		// Write data to sink.
-		if _, err := sink.Write(b); err != nil {
-			return err
-		}
-
-		// Close the sink.
-		if err := sink.Close(); err != nil {
-			return err
-		}
-
-		return nil
-	}()
-
-	if err != nil {
-		sink.Cancel()
-		return err
-	}
-
-	return nil
-}
-
-func (f *fsmSnapshot) Release() {}
